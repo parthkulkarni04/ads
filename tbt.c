e36be2e4549e5fb3497bf3fe62f8cbd2e4714ac9
Parth Kulkarni <114288341+parthkulkarni04@users.noreply.github.com>
2023-12-01 00:45:26 +0530

    Update tbt.c

--- tbt.c
+++ tbt.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure to represent a node in the threaded binary tree
+// Structure for a node in the threaded binary tree
 struct Node {
     int data;
     struct Node* left;
     struct Node* right;
-    int isThreaded; // Flag to indicate whether the right pointer is a thread
+    int isThreaded; // Flag to indicate whether right pointer is a thread or a child
 };
 
 // Function to create a new node
@@ -15,115 +15,87 @@ struct Node* createNode(int data) {
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
-    newNode->isThreaded = 0; // Initially, the right pointer is not a thread
+    newNode->isThreaded = 0; // Initially, the right pointer is a child, not a thread
     return newNode;
 }
 
-// Function to perform inorder threading
-void threadedInorder(struct Node* root, struct Node** prev) {
-    if (root != NULL) {
-        // Recur for the left subtree
-        threadedInorder(root->left, prev);
+// Function to perform Inorder threading
+void inorderThread(struct Node* root, struct Node** prev) {
+    if (root) {
+        inorderThread(root->left, prev);
 
-        // If the current node's left child is NULL, thread it to the predecessor
-        if (root->left == NULL) {
-            root->left = *prev;
+        // If the previous node is threaded, make it point to the current node
+        if (*prev && (*prev)->right == NULL) {
+            (*prev)->right = root;
+            (*prev)->isThreaded = 1;
+        }
+
+        // Update the previous pointer
+        *prev = root;
+
+        // If the right child is NULL, make it threaded to the inorder successor
+        if (root->right == NULL) {
+            root->right = *prev;
             root->isThreaded = 1;
         }
 
-        // If the predecessor's right child is NULL, thread it to the current node
-        if (*prev != NULL && (*prev)->right == NULL && !(*prev)->isThreaded) {
-            (*prev)->right = root;
-        }
-
-        // Update the predecessor to the current node
-        *prev = root;
-
-        // Recur for the right subtree
-        threadedInorder(root->right, prev);
+        inorderThread(root->right, prev);
     }
 }
 
-// Function to traverse the threaded binary tree in postorder
-void threadedPostorder(struct Node* root) {
-    if (root != NULL) {
-        // Recur for the left subtree
-        threadedPostorder(root->left);
+// Function to traverse the threaded binary tree in Inorder
+void inorderTraversal(struct Node* root) {
+    while (root) {
+        // Find the leftmost node
+        while (root->left)
+            root = root->left;
 
-        // Recur for the right subtree
-        threadedPostorder(root->right);
-
-        // Process the current node
+        // Print the current node's data
         printf("%d ", root->data);
-    }
-}
-
-// Function to traverse the threaded binary tree in preorder
-void threadedPreorder(struct Node* root) {
-    if (root != NULL) {
-        // Process the current node
-        printf("%d ", root->data);
-
-        // Recur for the left subtree
-        threadedPreorder(root->left);
-
-        // Recur for the right subtree
-        threadedPreorder(root->right);
-    }
-}
-
-// Function to traverse the threaded binary tree in inorder
-void threadedInorderTraversal(struct Node* root) {
-    struct Node* current = root;
-
-    // Find the leftmost node in the threaded binary tree
-    while (current->left != NULL)
-        current = current->left;
-
-    // Traverse the threaded binary tree
-    while (current != NULL) {
-        printf("%d ", current->data);
 
         // If the right pointer is a thread, move to the inorder successor
-        if (current->isThreaded)
-            current = current->right;
+        if (root->isThreaded)
+            root = root->right;
         else {
-            // Move to the leftmost node of the right subtree
-            current = current->right;
-            while (current != NULL && current->left != NULL)
-                current = current->left;
+            // Otherwise, move to the leftmost node of the right subtree
+            root = root->right;
+            while (root && !root->isThreaded)
+                root = root->left;
         }
     }
 }
 
-// Example usage
+// Function to traverse the threaded binary tree in Postorder
+void postorderTraversal(struct Node* root) {
+    if (root) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+
+        // Print the current node's data
+        printf("%d ", root->data);
+    }
+}
+
 int main() {
-    // Create nodes for the threaded binary tree
+    // Create a sample threaded binary tree
     struct Node* root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
     root->left->left = createNode(4);
     root->left->right = createNode(5);
-    root->right->left = createNode(6);
-    root->right->right = createNode(7);
 
-    // Perform inorder threading
+    // Perform Inorder threading
     struct Node* prev = NULL;
-    threadedInorder(root, &prev);
+    inorderThread(root, &prev);
 
-    // Traverse the threaded binary tree in postorder
-    printf("Postorder Traversal: ");
-    threadedPostorder(root);
-    printf("\n");
-
-    // Traverse the threaded binary tree in preorder
-    printf("Preorder Traversal: ");
-    threadedPreorder(root);
-    printf("\n");
-
-    // Traverse the threaded binary tree in inorder
+    // Traverse the threaded binary tree in Inorder
     printf("Inorder Traversal: ");
-    threadedInorderTraversal(root);
+    inorderTraversal(root);
+    printf("\n");
+
+    // Traverse the threaded binary tree in Postorder
+    printf("Postorder Traversal: ");
+    postorderTraversal(root);
     printf("\n");
 
     return 0;
